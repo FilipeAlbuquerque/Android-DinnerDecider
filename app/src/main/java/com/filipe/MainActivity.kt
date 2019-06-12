e9d6d23673d978817b2dca032510a18ac138fb68
FilipeAlbuquerque <filipefragosoti@gmail.com>
2019-06-12 11:25:38 -0300

    Added events clicked listener to the button and plain text

--- app/src/main/java/com/filipe/MainActivity.kt
+++ app/src/main/java/com/filipe/MainActivity.kt
@@ -3,13 +3,27 @@ package com.filipe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Chinese","Hamburger", "Pizza", "Açaí", "Tapioca", "Cuscuz")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        selectedFoodTxt.text = "Burger King"
+        decide_button.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFood]
+        }
+
+        addFoodButton.setOnClickListener {
+            val newFood = addFoodTxt.text.toString()
+            foodList.add(newFood)
+            addFoodTxt.text.clear()
+        }
     }
 }
